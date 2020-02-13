6c598dbda58b324e82edee8fa19b61acb3542262
Patrick Dunn <pmdunn78@gmail.com>
2020-02-13 15:06:07 -0500

    posting note with id to db.json

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
+var performanceNow = require("performance-now");
 
 
 const app = express();
@@ -22,7 +23,10 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
 
-var db = fs.readFile("./db/db.json", (err, data) => {
+var db = [];
+fs.readFile("./db/db.json", (err, data) => {
+  if (err) throw err;
+
   db = JSON.parse(data);
 }); 
 
@@ -32,8 +36,24 @@ app.get("/api/notes", (req, res) => {
 
 });
 
+// ------------------------------------------------------
+app.post("/api/notes", (req, res) => {
+  console.log(req.body)
+  var id = {"id":performanceNow()};
+
+  var data = { ...req.body, ...id};
+  db.push(data);
+
+  fs.writeFile("./db/db.json", JSON.stringify(db), (err) => {
+    if (err) throw err;
+  });
+
+  res.json(db);
+});
+
+
 
 // Starts the server to begin listening
-app.listen(PORT => {
+app.listen(PORT, () => {
   console.log("App listening on PORT " + PORT);
 });
